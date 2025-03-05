752c894eabde238c8c681a372c48d186e3b3e400
EcliqseX <127792164+EcliqseX@users.noreply.github.com>
2025-03-05 05:34:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ document.getElementById("setCustomTimeButton").addEventListener("click", setCust
 document.getElementById("timerModeButton").addEventListener("click", switchToTimerMode);
 // Set default background image when the page loads
 document.body.style.backgroundImage = "url('https://s7.ezgif.com/tmp/ezgif-78915635e4e077.gif')";
-document.body.style.backgroundSize = "80%";  // Shrinks the background to 80% of the screen
+document.body.style.backgroundSize = "120%";  // Shrinks the background to 80% of the screen
 document.body.style.backgroundRepeat = "no-repeat";
 document.body.style.backgroundPosition = "center";
 document.body.style.backgroundAttachment = "fixed"; // Keeps the background fixed when scrolling
